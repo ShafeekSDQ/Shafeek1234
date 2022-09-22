02e92f6c009d7115b694bbc104ce7c304a126d23
Shafeek <110275614+ShafeekSDQ@users.noreply.github.com>
2022-09-22 14:46:00 +0530

    Add files via upload

---
+++ src/components/Social.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Social extends Component {
+    render() {
+        return (
+            <div class="social">
+            <a href="https://codepen.io/abhinav3432" target="_blank" rel="noopener noreferrer"><i class="fab fa-codepen"></i></a>
+            <a href="https://github.com/abhinav03432" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i></a>
+            <a href="https://www.instagram.com/_.the_rebel._/" target="_blank" rel="noopener noreferrer"><i class="fab fa-instagram"></i></a>
+            <a href="https://www.facebook.com/Shafeek Sidhiq/" target="_blank" rel="noopener noreferrer"><i class="fab fa-facebook-f"></i></a>
+            <a href="https://Linkedin.com/in/Shafeek/" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin-in"></i></a>
+            <a href="https://medium.com/@Shafeek19" target="_blank" rel="noopener noreferrer"><i class="fab fa-medium-m"></i></a>
+            </div>
+            )
+        }
+    }
+    
+export default Social
+    
